85f4af5ccace87094efd74ecdde0d7f6bdf3b53a
c01nd01r <stas.er.it@gmail.com>
2018-11-21 20:27:25 +0300

    wip

--- .history/demo_20181121200015.js
+++
@@ -1,2 +0,0 @@
-import { validator, createRule } from './index';
-
--- .history/demo_20181121200033.js
+++
@@ -1,6 +0,0 @@
-import { validator, createRule } from './index';
-
-const max = createRule('Число больше, чем должно быть', (max, msg, value) =>
-  max < value ? msg : createRule.success
-);
-
